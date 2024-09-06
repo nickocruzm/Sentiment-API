bf5c778941013baaff7ccef75c30073ce9e13e0b
Nicko Cruz M <nickocruzm@gmail.com>
2024-09-06 14:17:58 -0700

    path change

--- main.py
+++ main.py
@@ -9,19 +9,13 @@ async def docs_redirect():
     response = RedirectResponse(url='/docs')
     return response
 
-from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
-import joblib
-
-app = FastAPI()
-
 @app.get("/prediction/", tags=["Prediction"])
 def get_prediction(review: str):
     """
     Endpoint to get sentiment prediction for a review text.
     """
-    vectorizer = joblib.load("/usercode/vectorizer.joblib")
-    model = joblib.load('/usercode/model.pkl')
+    vectorizer = joblib.load("vectorizer.joblib")
+    model = joblib.load('model.pkl')
     data = vectorizer.transform([review])
     response =  model.predict(data)
     sentiments = ['negative', 'neutral', 'positive']
